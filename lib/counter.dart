1d29f74f1f5f1f96f90be507290feeae4b9b53dd
modoo.jhy <modoo.jhy@gmail.com>
2024-05-13 21:54:53 +0900

    saveCount test complete

--- lib/counter.dart
+++ lib/counter.dart
@@ -3,16 +3,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Counter {
   int count = 0;
   late Timer _timer;
-
-
   Timer get timer => _timer;
+  SharedPreferences? sharePreference;
+  Counter([this.sharePreference]);
   
   void incrementCounter() {
     count++;
   }
 
   void decreaseCounter() {
-    count;
+    count--;
   }
   void startDecreaseTimer(){
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
@@ -22,17 +22,23 @@ class Counter {
 
   Future<void> saveCount() async {
     try{
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setInt('counter', count);
+      final prefs=sharePreference??await SharedPreferences.getInstance();
+      
+      final result=await prefs.setInt('counter', 3);
+      if(!result){
+        throw('setInt return false');
+      }
+      
     }catch(e){
+      print(e);
       throw(Exception('local storage 저장에 문제뱔생'));
     }
-    
   }
 
   Future<void> loadCount() async {
-    final prefs = await SharedPreferences.getInstance();
+    final prefs=sharePreference??await SharedPreferences.getInstance();
     count = prefs.getInt('counter') ?? 0;
+    
   }
 }
 
--- test/local_storage_test.dart
+++ test/local_storage_test.dart
@@ -1,58 +1,38 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:od_counter/counter.dart';
-import 'package:od_counter/mock_shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+@GenerateNiceMocks([MockSpec<SharedPreferences>()])
+import 'local_storage_test.mocks.dart';
 
 main() {
+  late MockSharedPreferences mockPrefs;
   late Counter counter;
+
   setUpAll(() {
-    counter = Counter();
+    mockPrefs = MockSharedPreferences();
+    counter = Counter(mockPrefs);
+    when(mockPrefs.setInt(any, any)).thenAnswer((_) async => true);
   });
-  
-  group('Counter Tests with SharedPreferences', () { 
-     late MockSharedPreferences mockPrefs;
-      late Counter counter;
 
-      setUp(() async {
-        mockPrefs = MockSharedPreferences();
-        SharedPreferences.setMockInitialValues({}); // Important: set initial values
-        // Ensure SharedPreferences.getInstance() returns the mock
-        when(SharedPreferences.getInstance()).thenAnswer((_) async => mockPrefs);
-        counter = Counter();
-        await counter.loadCount(); // Load initial count at the start of each test
-      });
-
-     group('count 값 read', () { 
-      test('SharedPreference에 count값을 성공적으로 load합니다.', () {
-        final readCount=counter.loadCount();
-        expect(readCount, isA<int>());
-      });
-     
-    });
-    group('count 값 저장', () { 
-     
-
-      test('SharedPreference에 count값을 성공적으로 저장합니다.', () async{
-        counter.count=3;
-        counter.saveCount();
-
-        final readCount=counter.loadCount();
-        expect(readCount, 3);
-
-      });
-      test('SharedPreference에 count값 저장시 exception 발생합니다.', () {
-        //강제로 exception은 어떻게 만들지?
-      });
+  group('count 값 read', () {
+    test('SharedPreference에 count값을 성공적으로 load합니다.', () async {
+      await counter.loadCount();
+      expect(counter.count, isA<int>());
     });
   });
- 
+  group('count 값 save', () {
+
+    test('SharedPreference에 count값을 성공적으로 저장합니다.', () async {
+      counter.count = 3;
+      await counter.saveCount();
+      verify(mockPrefs.setInt('counter', 3)).called(1);
+    });
+    test('SharedPreference에 count값 저장시 exception 발생합니다.', () {
+      //강제로 exception은 어떻게 만들지?
+    });
+  });
 }
-
-
-
-    // test('count값을 local에서 읽어옴. default 0', () async{
-      
-    // });
-    // test('count값을 local storage에 저장함', () async{
-      
-    // });
 
